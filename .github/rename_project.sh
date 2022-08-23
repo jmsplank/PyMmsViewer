4f2334ffe4bb3997a009955d2136b5f2469b73d8
jmsplank <jmsplank@users.noreply.github.com>
2022-08-23 11:22:15 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="jmsplank"
+original_name="pymmsviewer"
+original_urlname="PyMmsViewer"
+original_description="Awesome pymmsviewer created by jmsplank"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv pymmsviewer $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
